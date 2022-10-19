9f002e8d95366d750c5213b6dee8e5cc1e8717e5
Michał Grabka <73099710+Majkoo@users.noreply.github.com>
2022-10-19 20:05:29 +0200

    AddDocumentAsync method adjustment

--- DocumentsApp.Data/Services/Interfaces/IDocumentService.cs
+++ DocumentsApp.Data/Services/Interfaces/IDocumentService.cs
@@ -9,7 +9,7 @@ public interface IDocumentService
     Task<GetDocumentDto> GetDocumentByIdAsync(string id);
     Task<PagedResults<GetDocumentDto>> GetAllDocumentsAsync(SieveModel query);
     Task<PagedResults<GetDocumentDto>> GetAllSharedDocumentsAsync(SieveModel query);
-    Task<string> AddDocumentAsync(AddDocumentDto dto);
+    Task<GetDocumentDto> AddDocumentAsync(AddDocumentDto dto);
     Task UpdateDocumentAsync(string id, UpdateDocumentDto dto);
     Task DeleteDocumentAsync(string id);
 }
