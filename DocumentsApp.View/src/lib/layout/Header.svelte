308ecd1286efaea2d19964c2878c6d63ef0b7271
Michał Grabka <mr.grabka@gmail.com>
2023-08-06 12:34:37 +0200

    created UI skeleon

---
+++ DocumentsApp.View/src/lib/layout/Header.svelte
@@ -0,0 +1,10 @@
+<header
+		class="text-primary m-0"
+>
+	<div class="container-fluid d-flex align-items-center h-100 justify-content-end px-4 m-0">
+		<a href="/account" class="nav-link">Account</a>
+		<a href="/settings" class="nav-link">Settings</a>
+		<a href="/help" class="nav-link">Help</a>
+	</div>
+</header>
+
---
+++ DocumentsApp.View/src/lib/layout/Sidenav.svelte
@@ -0,0 +1,43 @@
+<aside class="d-flex flex-column text-white">
+
+	<nav class="h-100 d-flex flex-column">
+		<div class="side-title w-100">
+			<a
+					href="/"
+					class="w-100">
+				<h3 class="m-0">DocumentsApp</h3>
+			</a>
+		</div>
+
+		<div class="d-flex flex-column side-menu">
+			<a href="/" class="nav-link">
+				Documents
+			</a>
+			<a href="/create" class="nav-link">
+				Create
+			</a>
+			<a href="/share" class="nav-link">
+				Share
+			</a>
+		</div>
+
+		<div class="d-flex flex-column justify-content-end side-menu flex-fill">
+			<a href="/logout" class="nav-link">
+				Logout
+			</a>
+		</div>
+	</nav>
+
+
+
+</aside>
+
+<style lang="scss">
+  .sidenav-link {
+	text-decoration: none;
+	padding: 0 1rem;
+	&:hover {
+      opacity: 0.6;
+	}
+  }
+</style>
