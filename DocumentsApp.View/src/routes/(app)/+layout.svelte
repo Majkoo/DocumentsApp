faa6eb85dc258c4e5a8722027f3fd3b6cfb4aba2
Michał Grabka <mr.grabka@gmail.com>
2023-08-06 19:40:42 +0200

    added responsive layout

---
+++ DocumentsApp.View/src/routes/(app)/+layout.svelte
@@ -0,0 +1,30 @@
+<script>
+	import Header from "$lib/layout/Header.svelte";
+    import DocNav from "$lib/layout/DocNav.svelte";
+</script>
+
+
+<div class="body d-flex flex-column">
+
+	<Header />
+
+	<main class="d-flex flex-column flex-lg-row">
+		<DocNav />
+
+		<div class="page-content">
+			<slot />
+		</div>
+	</main>
+
+
+</div>
+
+<style global>
+	@import "../../app.scss";
+	.body {
+        height: 100vh;
+	}
+	main {
+		flex-grow: 1;
+	}
+</style>
