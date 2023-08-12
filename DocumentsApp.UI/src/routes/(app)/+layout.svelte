0fbb17b116dae0aeed3ebbe4e24f7a5c061bd267
Michał Grabka <73099710+Majkoo@users.noreply.github.com>
2023-08-12 23:42:31 +0200

    UI skeleton (#17)
    * added api project
    
    * Made basic auth controllers work
    
    * added refresh tokens
    
    * added refresh tokens
    
    * lockout fix
    
    * finished moving .View to .Api
    
    * finished moving .View to .Api
    
    * created svelte view project
    
    * created svelte view project
    
    * created UI skeleon
    
    * added icons
    
    * fixed help sidenav
    
    * changed documents icon
    
    * added responsive layout
    
    * changed color pallette
    
    * fix merge
    
    * added tailwindcss

---
+++ DocumentsApp.UI/src/routes/(app)/+layout.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+	import "../../app.css"
+    import { page } from '$app/stores';
+
+    $: currentRoute = $page.url.pathname;
+</script>
+
+<div class="flex flex-col h-full">
+
+	<header>
+		<a class="title" href="/">
+			<span class="material-icons-outlined">description</span>
+			DocumentsApp
+		</a>
+
+		<div class="flex">
+			<a class="header-link" href="/account" class:active={currentRoute === '/account'}>
+				<span class="material-icons-outlined">person</span>
+			</a>
+			<div class="header-link">
+				<span class="material-icons-outlined">light_mode</span>
+			</div>
+		</div>
+	</header>
+
+	<main>
+
+		<aside>
+			<div class="flex-grow">
+
+				<a class="nav-link" href="/my-items" class:active={currentRoute === '/my-items'}>
+					My items
+					<span class="material-icons-outlined">folder</span>
+				</a>
+
+				<a class="nav-link" href="/create" class:active={currentRoute === '/create'}>
+					Create
+					<span class="material-icons-outlined">add</span>
+				</a>
+
+				<a class="nav-link" href="/share" class:active={currentRoute ===  '/share'}>
+					Share
+					<span class="material-icons-outlined">share</span>
+				</a>
+
+			</div>
+
+			<a class="nav-link" href="/help" class:active={currentRoute ===  '/help'}>
+				<span class="w-full">Need some help</span>
+				<span class="material-icons-outlined">question_mark</span>
+			</a>
+
+		</aside>
+
+		<section class="content">
+
+			<slot />
+
+		</section>
+
+	</main>
+
+</div>
+
+
+<style global>
+    @import "../../../node_modules/material-icons";
+</style>
---
+++ DocumentsApp.UI/src/routes/(app)/+page.svelte
@@ -0,0 +1,4 @@
+<p>
+	My page content
+</p>
+
